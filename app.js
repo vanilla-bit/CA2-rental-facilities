844787242d9ee81addd3ab6d10271ea8c22c17d5
tyuyao <24019872@myrp.edu.sg>
2025-07-28 17:22:30 +0800

    8th

--- app.js
+++ app.js
@@ -833,7 +833,3 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on URL address: http://localhost:${PORT}/`));
 //end
-
-// Start server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on URL address: http://localhost:${PORT}/`));
